14b4a578369154e4ce436076b012d1e6c1b1bc95
Luhz <1393454295@qq.com>
2024-08-10 22:23:40 +0800

    新增了BetaLib，主要是用于快速测试某些功能

---
+++ Plugins/BetaLib/Source/BetaLib/Private/BetaLibBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BetaLibBPLibrary.h"
+#include "BetaLib.h"
+
+UBetaLibBPLibrary::UBetaLibBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UBetaLibBPLibrary::BetaLibSampleFunction(float Param)
+{
+	return -1;
+}
+
